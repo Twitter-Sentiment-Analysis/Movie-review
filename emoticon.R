131672f1ff0bc148f3e96f941beff1aa41318683
Sonali Agrawal <sonali9696@gmail.com>
2016-09-13 00:27:19 +0530

    http remove

--- emoticon.R
+++ emoticon.R
@@ -1,3 +1,6 @@
 df <- do.call("rbind", lapply(junglebook.tweets, as.data.frame))
+#remove emoticons
 df$text <- sapply(df$text,function(row) iconv(row, "latin1", "ASCII", sub=""))
+#remove URLs
+df$text = gsub("(f|ht)tp(s?)://(.*)[.][a-z]+", "", df$text)
 sample <- df$text
